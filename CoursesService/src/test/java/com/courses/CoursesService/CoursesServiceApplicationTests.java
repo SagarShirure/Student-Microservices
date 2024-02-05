9d35233ce60a263ef79fcd01c17e01ea0717ed0d
Sagar Devidas Shirure <sagarshirure145@gmail.com>
2024-02-05 14:08:08 +0530

    course ms

---
+++ CoursesService/src/test/java/com/courses/CoursesService/CoursesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.courses.CoursesService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
